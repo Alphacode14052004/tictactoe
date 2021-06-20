6c587fed5b0b6926cf0a71ac171bd7bace5fe556
Alphacode123456789 <jimvenkat999@gmail.com>
2021-06-20 11:04:45 -0700

    css started

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import './Square.Style.css';
 
-const Square = props => {
-  console.log(props);
-  return <button type="button">{props.value}</button>;
+const Square = ({ value }) => {
+  return <button type="button">{value}</button>;
 };
 
 export default Square;
